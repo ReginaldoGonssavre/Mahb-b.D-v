1aa39a9113a400fba6ad4f349ff1902610d24acb
Serena <serena.agronovatech@gmail.com>
2025-06-26 15:30:06 -0300

    Add basic marketing website for Aigro Quantum SaaS

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Obrigado pelo seu interesse! Em breve, mais informações sobre a Aigro Quantum SaaS.');
+});
